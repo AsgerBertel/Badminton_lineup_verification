3ae9688d00572916cc54dfacebc0367189a43fb3
KristianOdum <47847418+KristianOdum@users.noreply.github.com>
2020-09-11 14:17:04 +0200

    Choose player popup: Search also adhere to predicate from parameter

--- src/main/java/gui/view/ChoosePlayerFragment.kt
+++ src/main/java/gui/view/ChoosePlayerFragment.kt
@@ -2,6 +2,7 @@ package gui.view
 
 import javafx.beans.property.SimpleObjectProperty
 import javafx.collections.ObservableList
+import javafx.collections.transformation.FilteredList
 import javafx.collections.transformation.SortedList
 import javafx.scene.control.TableRow
 import javafx.util.Duration
@@ -18,30 +19,29 @@ class ChoosePlayerFragment(players: ObservableList<Player>, predicate: (Player) 
     private var resultPlayer:Player? = null
 
     init {
-        val filteredPlayers = players.filtered { predicate(it)}
+        val filteredPlayers = FilteredList(players)
+        filteredPlayers.setPredicate(predicate)
+
         textfield {
             promptText = "Search for name or ID";
             isFocusTraversable = false;
 
-            textProperty().addListener { observable, oldValue, newValue ->
+            textProperty().addListener { _, _, newValue ->
                 filteredPlayers.setPredicate { myObject ->
-                    // If filter text is empty, display all players
-                    if (newValue == null || newValue.isEmpty()) {
-                        return@setPredicate true
-                    }
-
                     // Compare name field in object with filter.
                     val lowerCaseFilter: String = newValue.toLowerCase()
-                    if (java.lang.String.valueOf(myObject.name).toLowerCase().contains(lowerCaseFilter)) {
-                        return@setPredicate true
-                        // Filter matches name.
-                    }
 
-                    // Compare badmintonID field in object with filter.
-                    if(myObject.badmintonId.toString().contains(lowerCaseFilter)){
-                        return@setPredicate true
+                    // Must adhere to the original predicate from parameter
+                    if(predicate(myObject)) {
+                        // Filter matches name.
+                        if (java.lang.String.valueOf(myObject.name).toLowerCase().contains(lowerCaseFilter))
+                            return@setPredicate true
+
+                        // Compare badmintonID field in object with filter.
+                        if (myObject.badmintonId.toString().contains(lowerCaseFilter))
+                            return@setPredicate true
                     }
-                    false
+                    return@setPredicate false
                 }
             }
 
@@ -60,20 +60,19 @@ class ChoosePlayerFragment(players: ObservableList<Player>, predicate: (Player) 
             readonlyColumn("Double Points", Player::doublesPoints)
             readonlyColumn("Mixed Points", Player::mixedPoints)
             bindSelected(selectedPlayerProperty)
+
             setRowFactory {
                 val row: TableRow<Player> = TableRow()
                 row.setOnMouseClicked { event ->
-                    if (event.getClickCount() === 2 && !row.isEmpty()) {
+                    if (event.clickCount == 1 && !row.isEmpty) {
                         resultPlayer = selectedPlayer
-                        runLater(Duration.millis(100.0)) {
+                        runLater(Duration.millis(60.0)) {
                             close()
                         }
                     }
                 }
                 row
             }
-
-
         }
 
     }
