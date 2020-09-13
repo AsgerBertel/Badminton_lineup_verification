6885c83c386616350f8d4c390cb6a1f6bcc0bb65
KristianOdum <47847418+KristianOdum@users.noreply.github.com>
2020-09-13 20:24:15 +0200

    Cleanup smol

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -3,10 +3,6 @@ import model.Player
 import tornadofx.launch
 
 fun main(args: Array<String>) {
-    //val players = JsonFileHandler().loadPlayerFile("""C:\git\Team-match-verify\src\main\resources\PlayerList.json""")
-    //val players = RankListScraper().scrapeRankList()
 
     launch<MyApp>(args)
 }
-
-fun getPlayer(name:String, players:List<Player>) = players.find { it.name == name } ?: Player()
